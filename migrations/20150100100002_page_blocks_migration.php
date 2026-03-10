b6285dd6959efe3b5224911cb94661c995c376a3
Chris Burton <chris.burton@revolutionbarsgroup.com>
2026-03-10 19:57:01 +0000

    Updates

--- migrations/20150100100002_page_blocks_migration.php
+++ migrations/20150100100002_page_blocks_migration.php
@@ -14,15 +14,47 @@ final class PageBlocksMigration extends BasePantonoMigration
             ->create();
 
         $this->insertOnCreate($this->addTablePrefix('page_block_type'), [
-            ['name' => 'Text', 'fields' => json_encode([['name' => 'content', 'type' => 'html']])],
-            ['name' => 'Image', 'fields' => json_encode([['name' => 'content', 'type' => 'image']])]
+            [
+                'id' => 1,
+                'name' => 'Row',
+                'fields' => json_encode([
+                    ['name' => 'columns', 'type' => 'integer', 'default' => 12],
+                    ['name' => 'gap', 'type' => 'integer', 'default' => 0],
+                ]),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Item',
+                'fields' => json_encode([
+                    ['name' => 'column_span', 'type' => 'integer', 'default' => 12],
+                    ['name' => 'column_start', 'type' => 'integer', 'default' => 1],
+                ]),
+            ],
+            ['id' => 3, 'name' => 'Text', 'fields' => json_encode([['name' => 'content', 'type' => 'html']])],
+            ['id' => 4, 'name' => 'Image', 'fields' => json_encode([['name' => 'content', 'type' => 'image']])],
         ]);
+
+        $this->table($this->addTablePrefix('page_block_type_child'), ['id' => false])
+            ->addLinkedColumn('parent_block_type_id', $this->addTablePrefix('page_block_type'), 'id')
+            ->addLinkedColumn('child_block_type_id', $this->addTablePrefix('page_block_type'), 'id')
+            ->addIndex(['parent_block_type_id', 'child_block_type_id'], ['unique' => true])
+            ->create();
+
+        $this->insertOnCreate($this->addTablePrefix('page_block_type_child'), [
+            ['parent_block_type_id' => 1, 'child_block_type_id' => 2], // Row -> Item
+            ['parent_block_type_id' => 2, 'child_block_type_id' => 3], // Item -> Text
+            ['parent_block_type_id' => 2, 'child_block_type_id' => 4], // Item -> Image
+        ], false);
 
         $this->table($this->addTablePrefix('page_block'))
             ->addLinkedColumn('page_version_id', $this->addTablePrefix('page_version'), 'id')
+            ->addLinkedColumn('parent_block_id', $this->addTablePrefix('page_block'), 'id', ['null' => true], ['delete' => 'SET NULL'])
             ->addLinkedColumn('block_type_id', $this->addTablePrefix('page_block_type'), 'id')
-            ->addColumn('content', 'json')
-            ->addColumn('display_order', 'integer')
+            ->addColumn('content', 'json', ['null' => true])
+            ->addColumn('settings', 'json', ['null' => true])
+            ->addColumn('display_order', 'integer', ['default' => 0])
+            ->addIndex(['page_version_id', 'parent_block_id', 'display_order'])
+            ->addIndex(['parent_block_id', 'display_order'])
             ->create();
     }
 }
